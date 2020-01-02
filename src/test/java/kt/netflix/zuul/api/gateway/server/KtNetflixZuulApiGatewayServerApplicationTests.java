86c3f5ca292d4e84c029d9d635788deb2d48aa4e
karnakar <karnakar@ymail.com>
2020-01-01 22:18:37 -0500

    first commit

---
+++ src/test/java/kt/netflix/zuul/api/gateway/server/KtNetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kt.netflix.zuul.api.gateway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtNetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
