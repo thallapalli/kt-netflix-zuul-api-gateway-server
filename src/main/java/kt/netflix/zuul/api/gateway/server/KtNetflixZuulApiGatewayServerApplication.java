8b1cb6ab95b4c065faf0912a94a9f343f3a64a78
karnakar <karnakar@ymail.com>
2020-01-01 22:51:41 -0500

    sleuth added

--- src/main/java/kt/netflix/zuul/api/gateway/server/KtNetflixZuulApiGatewayServerApplication.java
+++ src/main/java/kt/netflix/zuul/api/gateway/server/KtNetflixZuulApiGatewayServerApplication.java
@@ -4,14 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+
+
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+
 public class KtNetflixZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KtNetflixZuulApiGatewayServerApplication.class, args);
 	}
-
+	@Bean
+	public Sampler defalutSample() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+	
 }
